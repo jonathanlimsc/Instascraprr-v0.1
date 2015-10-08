7705a76593acf8af22eabc9b55aea2968ff2a79f
Jonathan Lim <jonathan@1sb.com>
2015-10-08 15:02:22 -0700

    Removed jquery for justified-gallery

--- pixlee.js
+++ pixlee.js
@@ -6,30 +6,6 @@ if (Meteor.isClient) {
       Session.set('postsCursor', result);
     });
 
-    //Gallery
-    $('#gallery').justifiedGallery({
-      // option: default,
-      rowHeight: 120,
-      maxRowHeight: 0,
-      lastRow: 'nojustify',
-      fixedHeight: false,
-      captions: true,
-      margins: 1,
-      randomize: false,
-      extension: /.[^.]+$/,
-      refreshTime: 250,
-      waitThumbnailsLoad: true,
-      justifyThreshold: 0.35,
-      cssAnimation: false,
-      imagesAnimationDuration: 300
-    }).on('jg.complete', function (e) {
-      // this callback runs after the gallery layout is created
-    }).on('jg.resize', function (e) {
-      // this callback runs after the gallery is resized
-    }).on('jq.rowflush', function (e) {
-      // this callback runs when a new row is ready
-    });
-
     //Datepicker
     Template.datepicker.rendered = function() {
         $('.input-daterange').datepicker({
